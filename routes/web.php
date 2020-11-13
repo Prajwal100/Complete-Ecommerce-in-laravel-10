17404824d133e26485ffb439ec5bfa10530b7f0a
Prajwal100 <iar.prajwal@gmail.com>
2020-11-13 23:53:41 +0545

    update

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/welcome',function(){
-    return view('welcome');
-});
 Auth::routes(['register'=>false]);
 
 Route::get('user/login','FrontendController@login')->name('login.form');
