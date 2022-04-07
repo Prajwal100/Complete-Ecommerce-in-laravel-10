0bcf27f21ae6f8c2d0518ad9ef870a60e1ae2ac4
Mouhcine <45631812+Crushoverride007@users.noreply.github.com>
2022-04-07 23:30:50 +0000

    adding htaccess

--- public/js/app.js
+++ public/js/app.js
@@ -55977,8 +55977,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Administrator\Desktop\laravelproject\Advance-Ecommerce-in-laravel-7\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Administrator\Desktop\laravelproject\Advance-Ecommerce-in-laravel-7\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Administrator\Desktop\laravelproject\finallaravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Administrator\Desktop\laravelproject\finallaravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
