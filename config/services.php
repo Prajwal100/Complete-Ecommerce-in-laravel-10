4192c944423dd35a530ef0e0c24b103050862b28
Prajwal Rai <prajwal.iar@gmail.com>
2023-09-21 07:52:00 +0545

    Update services.php

--- config/services.php
+++ config/services.php
@@ -30,18 +30,18 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'github' => [
-        'client_id' => '24fbd0edd46019058da7', //Github API
-        'client_secret' => '4500e6ab7a9bb70608600fbdee37969276bb0685', //Github Secret
+        'client_id' => 'YOUR_GITHUB_API', //Github API
+        'client_secret' => 'YOUR_GITHUB_SECRET', //Github Secret
         'redirect' => 'http://localhost:8000/login/github/callback',
      ],
      'google' => [
-        'client_id' => '122886334250-fsfd8ugugkr0hjrkakukbgnnruomjn95.apps.googleusercontent.com', //Google API
-        'client_secret' => 'eyuwbzXGBgbebzPXNiqSOods', //Google Secret
+        'client_id' => 'YOUR_GOOGLE_API', //Google API
+        'client_secret' => 'YOUR_GOOGLE_SECRET', //Google Secret
         'redirect' => 'http://localhost:8000/login/google/callback',
      ],
      'facebook' => [
-        'client_id' => '1494792524037846', //Facebook API
-        'client_secret' => '1bf13c2faf680f4ae2b97bdafa92c7d2', //Facebook Secret
+        'client_id' => 'YOUR_FACEBOOK_API', //Facebook API
+        'client_secret' => 'YOUR_FACEBOK_SECRET', //Facebook Secret
         'redirect' => 'http://localhost:8000/login/facebook/callback',
      ],
 
